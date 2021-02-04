eabf6510f2412f4e909d4c2b14bbf4a6a3b0aee7
toyocrabs <mljkprvt@gmail.com>
2021-02-04 18:37:06 +0800

    Added code which shows lat/lng when click on any part of the map

--- script.js
+++ script.js
@@ -10,7 +10,26 @@ function initMap() {
     let center = new google.maps.LatLng(1.3521, 103.8198);
     map = new google.maps.Map(document.getElementById("map"), {
       center: center,
-      zoom: 15,
+      zoom: 10,
+    });
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: "Click the map to get Lat/Lng!",
+      position: center,
+    });
+    infoWindow.open(map);
+    // Configure the click listener.
+    map.addListener("click", (mapsMouseEvent) => {
+      // Close the current InfoWindow.
+      infoWindow.close();
+      // Create a new InfoWindow.
+      infoWindow = new google.maps.InfoWindow({
+        position: mapsMouseEvent.latLng,
+      });
+      infoWindow.setContent(
+        JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+      );
+      infoWindow.open(map);
     });
   
     //User input has autocomplete
